6d035f39b7f2302b73c3eedb927164c5427796d2
ratataque <ewanrimmely@outlook.fr>
2022-12-07 16:18:13 +0100

    fix

--- app/src/main/java/com/applivroooom/Login.java
+++ app/src/main/java/com/applivroooom/Login.java
@@ -2,6 +2,10 @@ package com.applivroooom;
 
 import android.os.Bundle;
 import android.os.PersistableBundle;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,8 +16,20 @@ public class Login extends AppCompatActivity {
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
+        init();
     }
 
+    private EditText username;
+    private EditText password;
 
+
+    private void init() {
+        username = (EditText) findViewById(R.id.username_text);
+        password = (EditText) findViewById(R.id.password_text);
+
+        //this.controle = Controle.getInstance(this);
+        //ecouteCalcul();
+        //recupProfil();
+    }
 
 }
