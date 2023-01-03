69cd7c48c620312c3562bf4f9ee6e1b516cd748a
ratataque <ewanrimmely@outlook.fr>
2023-01-03 18:28:48 +0100

    new expertise

--- app/src/main/java/com/applivroooom/Rapport.java
+++ app/src/main/java/com/applivroooom/Rapport.java
@@ -1,8 +1,11 @@
 package com.applivroooom;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -21,7 +24,8 @@ public class Rapport extends AppCompatActivity {
         setContentView(R.layout.rapport);
 
         init();
-        //ecouteConfiramtion();
+//        ecouteEnvoie();
+        ecouteCreateExpertise();
     }
 
     private void init() {
@@ -31,4 +35,20 @@ public class Rapport extends AppCompatActivity {
 
     }
 
+    private void ecouteEnvoie() {
+
+    }
+
+    private void ecouteCreateExpertise() {
+        btn_ajout.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+                Toast.makeText(Rapport.this, "exp", Toast.LENGTH_SHORT).show();
+
+                Intent intent = new Intent(getApplicationContext(), New_expertise.class);
+                startActivity(intent);
+//                finish();
+            }
+        });
+    }
 }
